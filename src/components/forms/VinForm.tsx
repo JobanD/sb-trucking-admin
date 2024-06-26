766fb65b222ecf2b700852fb843dfffddca6d387
JobanD <joban.d555@gmail.com>
2024-06-25 23:48:18 -0400

    Updated import path for form

--- src/components/forms/VinForm.tsx
+++ src/components/forms/VinForm.tsx
@@ -11,9 +11,9 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
-import { Button } from "./ui/button";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
   vin: z.string(),
